e2e57005511825461f8d391c39825765830bb80d
Megan Weijiang <meganweijiang@utexas.edu>
2017-09-07 18:09:28 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  get 'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
+  resources :users
+end
